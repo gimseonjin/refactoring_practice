73fb87c7092de2b83f2454f601d0d668d3275be0
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-10-27 04:53:58 +0900

    ch1 create html form

--- ch1/main.js
+++ ch1/main.js
@@ -1,89 +1,105 @@
-var statment = (invoice) =>{
-
-    let result = `청구 내역 (고객명 : ${invoice.customer})\n`
-
-    for (let perf of invoice.performances){
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석) \n`
+class statement{
+    constructor(invoice, plays){
+        this.plays = plays
+        this.customer = invoice.customer
+        this.performances = invoice.performances.map(this.enrichPerformance)
+        this.totalAmountResult = this.totalAmount(this.performances)
+        this.totalVolumeCreditsResult = this.totalVolumeCredits(this.performances)
     }
 
-    result += `총액 : ${usd(totalAmount(invoice.performances))}\n`
-    result += `적립 포인트: ${totalVolumeCredits(invoice.performances)}점\n`
-
-    return result
-}
-
-var totalAmount = (aPerformance) => {
-
-    let result = 0
-
-    for (let perf of aPerformance){
-        result += amountFor(perf)
+    run = () => {
+        return this.renderHtml()
     }
+
+    renderPlainText = () => {
+        let result = `청구 내역 (고객명 : ${this.customer})\n`
+
+        for (let perf of this.performances){
+            result += ` ${perf.play.name}: ${this.usd(perf.amount)} (${perf.audience}석) \n`
+        }
     
-    return result
-}
-
-var totalVolumeCredits = (aPerformance) => {
-
-    let result = 0
-
-    for (let perf of aPerformance){
-        result  += volumeCreditsFor(perf) 
+        result += `총액 : ${this.usd(this.totalAmountResult)}\n`
+        result += `적립 포인트: ${this.totalVolumeCreditsResult}점\n`
+    
+        return result
     }
 
-    return result
-}
-
-var usd = (aPerformance) =>{
-    return new Intl.NumberFormat("en-US", { style : "currency", currency : "USD", minimumFractionDigits: 2})
-                    .format(aPerformance/100)
-}
-
-var volumeCreditsFor = (aPerformance) =>{
-
-    let volumeCredits = 0
-
-    volumeCredits += Math.max(aPerformance.audience - 30, 0)
-
-    if("comedy" == playFor(aPerformance).type) 
-        volumeCredits += Math.floor(aPerformance.audience / 5)
+    renderHtml = () => {
+        let result = `<h1>청구 내역 (고객명 : ${this.customer})</h1>\n`
+        result += `<table>\n`
+        result += `<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>`
+        for (let perf of this.performances){
+            result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}석</td><td>${this.usd(perf.amount)}</td>\n`
+        }
+        result += `</table>\n`
+        result += `<p>총액 : <em>${this.usd(this.totalAmountResult)}</em></p>\n`
+        result += `<p>적립 포인트: <em>${this.totalVolumeCreditsResult}</em>점</p>\n`
     
-    return volumeCredits
+        return result
+    }
 
-}
+    enrichPerformance = (aPerformance) => {
+        const result = Object.assign({}, aPerformance)
+        result.play = this.playFor(result)
+        result.amount = this.amountFor(result)
+        result.volumeCredits = this.volumeCreditsFor(result)
+        return result
+    }
 
-var playFor = (aPerformance) =>{
-    return playsJson[aPerformance.playID]
-}
+    totalAmount = () => this.performances.reduce((total, p) => total + p.amount, 0)
+    
+    totalVolumeCredits = () => this.performances.reduce((total, p) => total + p.volumeCredits, 0)
 
+    usd = (aPerformance) =>{
+        return new Intl.NumberFormat("en-US", { style : "currency", currency : "USD", minimumFractionDigits: 2})
+                        .format(aPerformance/100)
+    }
 
-var amountFor = (aPerformance) => {
+    volumeCreditsFor = (aPerformance) =>{
 
-    let result = 0;
-
-    switch (playFor(aPerformance).type){
-        case "tragedy":
-            result = 40000
-            if (aPerformance.audience > 30){
-                result += 1000 * (aPerformance.audience - 30)
-            }
-            break;
+        let volumeCredits = 0
+    
+        volumeCredits += Math.max(aPerformance.audience - 30, 0)
+    
+        if("comedy" == aPerformance.play.type) 
+            volumeCredits += Math.floor(aPerformance.audience / 5)
         
-        case "comedy":
-            result = 30000
-            if (aPerformance.audience > 20){
-                result += 10000 + 500 * (aPerformance.audience - 20)
-            }
-            result += 300 * aPerformance.audience
-            break;
-
-        default :
-            throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
+        return volumeCredits
+    
     }
 
-    return result
-}
+    playFor = (aPerformance) =>{
+        return this.plays[aPerformance.playID]
+    }
 
+    amountFor = (aPerformance) => {
+
+        let result = 0;
+
+        switch (aPerformance.play.type){
+
+            case "tragedy":
+                result = 40000
+                if (aPerformance.audience > 30){
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+                break;
+            
+            case "comedy":
+                result = 30000
+                if (aPerformance.audience > 20){
+                    result += 10000 + 500 * (aPerformance.audience - 20)
+                }
+                result += 300 * aPerformance.audience
+                break;
+    
+            default :
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
+        }
+    
+        return result
+    }
+}
 
 //================== main ==================
 
@@ -95,7 +111,8 @@ const invoices = fs.readFileSync('ch1/data/invoices.json', 'utf8');
 const playsJson = JSON.parse(plays);
 const invoicesJson = JSON.parse(invoices);
 
-for (let invoice of invoicesJson){
-    const result = statment(invoice, playsJson)
+for(let invoice of invoicesJson){
+    const s = new statement(invoice, playsJson)
+    const result = s.run()
     console.log(result)
 }
